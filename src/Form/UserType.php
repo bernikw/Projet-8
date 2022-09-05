ca62ee573641716759e6c3c4a4c628f77187dc3a
bernikw <bernikw@gmail.com>
2022-09-05 14:04:39 +0200

    Create roles, website design

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -19,14 +19,14 @@ class UserType extends AbstractType
     {
         $builder
         ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+        ->add('email', EmailType::class, ['label' => 'Adresse email'])
         ->add('password', RepeatedType::class, [
             'type' => PasswordType::class,
             'invalid_message' => 'Les deux mots de passe doivent correspondre.',
             'required' => true,
             'first_options'  => ['label' => 'Mot de passe'],
             'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
-        ])
-        ->add('email', EmailType::class, ['label' => 'Adresse email'])
+        ])       
         ->add('roles', CollectionType::class, [
             'entry_type'   => ChoiceType::class,
             'entry_options'  => [
